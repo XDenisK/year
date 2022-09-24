d5c872c6dd0ab02c8bce0122128f2d12293e52a2
PIBO0121032 Кошель Денис Алексеевич <PIBO0121032@sibupk.su>
2022-09-24 11:30:53 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+
+namespace Year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int a;
+            Console.WriteLine("Введите год");
+
+
+
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+            if (a % 4 == 0)
+            {
+                Console.WriteLine("Високосный год");
+            }
+            else
+            {
+                Console.WriteLine("Невисокосный год");
+            }
+        }
+    }
+}
